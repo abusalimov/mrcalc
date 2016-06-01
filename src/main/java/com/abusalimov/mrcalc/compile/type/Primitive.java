ba4f35eb10706a70263384f8c74e44dca3cf7b2d
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2016-06-01 15:20:57 +0300

    compile: (refactor) Extract Type interface & package
    The left enum is now called Primitive.

--- src/main/java/com/abusalimov/mrcalc/compile/type/Primitive.java
+++ src/main/java/com/abusalimov/mrcalc/compile/type/Primitive.java
@@ -1,4 +1,4 @@
-package com.abusalimov.mrcalc.compile;
+package com.abusalimov.mrcalc.compile.type;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,26 +7,31 @@ import java.util.List;
 /**
  * @author Eldar Abusalimov
  */
-public enum Type {
+public enum Primitive implements Type {
     INTEGER("integer"),
     FLOAT("float"),
     UNKNOWN("?");
 
     private final String name;
 
-    Type(String name) {
+    Primitive(String name) {
         this.name = name;
     }
 
-    public static Type promote(Type... types) {
+    public static Primitive promote(Primitive... types) {
         return promote(Arrays.asList(types));
     }
 
-    public static Type promote(List<Type> types) {
+    public static Primitive promote(List<Primitive> types) {
         if (types.size() > 0) {
             return Collections.max(types);
         }
         return UNKNOWN;
+    }
+
+    @Override
+    public Primitive getPrimitive() {
+        return this;
     }
 
     @Override
---
+++ src/main/java/com/abusalimov/mrcalc/compile/type/Type.java
@@ -0,0 +1,8 @@
+package com.abusalimov.mrcalc.compile.type;
+
+/**
+ * @author Eldar Abusalimov
+ */
+public interface Type {
+    Primitive getPrimitive();
+}

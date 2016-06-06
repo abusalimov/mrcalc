95366e953fcac8b37f809657537c29706e30644d
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2016-06-06 19:43:14 +0300

    ast: (cleanup) Remove variable linkage through AST nodes
    The linkage is now performed an maintained internally by the compiler
    as part of the type inference routine.
    
    This partially reverts commit 0cc2dd5 ("Link variable references to
    their definitions").

--- src/main/java/com/abusalimov/mrcalc/ast/expr/VarRefNode.java
+++ src/main/java/com/abusalimov/mrcalc/ast/expr/VarRefNode.java
@@ -3,19 +3,15 @@ package com.abusalimov.mrcalc.ast.expr;
 import com.abusalimov.mrcalc.ast.AbstractNode;
 import com.abusalimov.mrcalc.ast.NodeArgVisitor;
 import com.abusalimov.mrcalc.ast.NodeVisitor;
-import com.abusalimov.mrcalc.ast.stmt.VarDefStmtNode;
 
 /**
  * A variable reference must occur after the variable definition in the source code.
  * Compiler links the reference to its definition by the variable name and using scoping rules.
  *
- * @see #getLinkedDef()
  * @author Eldar Abusalimov
  */
 public class VarRefNode extends AbstractNode implements ExprNode {
     private String name;
-
-    private VarDefStmtNode linkedDef;
 
     public VarRefNode() {
     }
@@ -30,14 +26,6 @@ public class VarRefNode extends AbstractNode implements ExprNode {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public VarDefStmtNode getLinkedDef() {
-        return linkedDef;
-    }
-
-    public void setLinkedDef(VarDefStmtNode linkedDef) {
-        this.linkedDef = linkedDef;
     }
 
     @Override

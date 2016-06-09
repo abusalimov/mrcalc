6cfaf6cfe0a42444dd630d6b9c7f202ede85996f
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2016-06-09 16:31:00 +0300

    test: Switch Compiler test to JUnit4 style

--- src/test/groovy/com/abusalimov/mrcalc/CompilerTest.groovy
+++ src/test/groovy/com/abusalimov/mrcalc/CompilerTest.groovy
@@ -4,18 +4,20 @@ import com.abusalimov.mrcalc.compile.CompileErrorException
 import com.abusalimov.mrcalc.compile.Compiler
 import com.abusalimov.mrcalc.parse.Parser
 import com.abusalimov.mrcalc.parse.impl.antlr.ANTLRParserImpl
+import org.junit.Before
+import org.junit.Test
 
 /**
  * @author Eldar Abusalimov
  */
-class CompilerTest extends DiagnosticTestCase {
-    def diagnosticExceptionClass = CompileErrorException
+class CompilerTest {
+    def shouldDiagnose = DiagnosticAssert.&shouldDiagnose.curry CompileErrorException
 
     private Parser parser
     private Compiler compiler
 
+    @Before
     void setUp() {
-        super.setUp()
         parser = new ANTLRParserImpl()
         compiler = new Compiler()
     }
@@ -25,34 +27,40 @@ class CompilerTest extends DiagnosticTestCase {
         compiler.compile node
     }
 
-    void testCompilesExpressions() {
+    @Test
+    void "compiles simple math expressions"() {
         assert null != compile("0")
         assert null != compile("(1)")
         assert null != compile("(1+2)")
         assert null != compile("1+2-3*4/5^6")
     }
 
-    void testCompilesValidVarDefs() {
+    @Test
+    void "compiles valid variable definition statements"() {
         assert null != compile("var answer = 42")
         assert null != compile("var x = 0; var y = 1; var z = 3")
     }
 
-    void testCompilesValidVarRefs() {
+    @Test
+    void "compiles valid variable references"() {
         assert null != compile("var answer = 42; answer")
         assert null != compile("var x = 0; var y = 1; var z = 3; var foo = x+y+z")
     }
 
-    void "test error on duplicate variables"() {
+    @Test
+    void "reports error on duplicate variables"() {
         shouldDiagnose("already defined") { compile("var answer = 42; var answer = -1") }
     }
 
-    void "test error on undefined variables"() {
+    @Test
+    void "reports error on undefined variables"() {
         shouldDiagnose("undefined variable") { compile("unknown") }
         shouldDiagnose("undefined variable") { compile("var x = y + z") }
         shouldDiagnose("undefined variable") { compile("var foo = bar; var bar = foo") }
     }
 
-    void "test error on self-referencing variable"() {
+    @Test
+    void "reports error on self-referencing variable"() {
         shouldDiagnose("undefined variable") { compile("var r = r") }
     }
 }

473c8ed32415778d1df9a49579da8d6bb6bddd6f
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2016-06-14 12:53:11 +0300

    compile: Add javadocs for Compiler class

--- src/main/java/com/abusalimov/mrcalc/compile/Compiler.java
+++ src/main/java/com/abusalimov/mrcalc/compile/Compiler.java
@@ -18,6 +18,21 @@ import java.util.stream.Collectors;
 
 
 /**
+ * Compiler plays the essential role in transforming source code into a runnable function.
+ * <p>
+ * Compiler operates on statements level: it takes AST {@link StmtNode}s and turns them into {@link Stmt} instances. It
+ * compiles statements in two phases:
+ * <p>
+ * <li> First, it invokes {@link TypeInferrer#infer(ExprHolderNode, Map)} that fills in an {@link ExprTypeInfo} instance
+ * with a proper {@link com.abusalimov.mrcalc.compile.type.Type} of each sub-expression of the statement. The
+ * TypeInferrer is also responsible for reporting all the possible type errors that get emitted by the compiler.
+ * <p>
+ * <li> Next, it calls {@link ExprBuilder#buildFunction(ExprTypeInfo)} which, in turn, based on the inferred type of
+ * each expression, delegates to a proper {@code backend} expression builder to compose the expression into a callable.
+ * <p>
+ * A Compiler instance is stateful: it maintains and preserves a list of defined {@link Variable}s so that one can refer
+ * to a variable defined through another call to the {@link #compile(ProgramNode)} method.
+ *
  * @author Eldar Abusalimov
  */
 public class Compiler extends AbstractNodeDiagnosticEmitter {
@@ -33,11 +48,26 @@ public class Compiler extends AbstractNodeDiagnosticEmitter {
         this(new FuncExprFactoryImpl());
     }
 
+    /**
+     * Creates a new Compiler instance that will use the specified backend to compose the resulting callable.
+     *
+     * @param exprFactory the backend implementation
+     */
     public Compiler(ExprFactory<?> exprFactory) {
         typeInferrer = new TypeInferrer();
         exprBuilder = new ExprBuilder<>(exprFactory);
     }
 
+    /**
+     * Compiles an AST root into a list of {@link Stmt#exec(Map) executable} statements.
+     * <p>
+     * Calling this method is almost identical to invoking {@link #compile(StmtNode)} multiple times, except that all
+     * diagnostics reported, if any, are composed into a single {@link CompileErrorException exception} instance.
+     *
+     * @param node the AST root node
+     * @return the list of {@link Stmt statements}
+     * @throws CompileErrorException in case the AST has semantic errors, like type mismatch
+     */
     public List<Stmt> compile(ProgramNode node) throws CompileErrorException {
         try (DiagnosticCollectorCloseable<CompileErrorException> diagnosticsToThrow =
                      collectDiagnosticsToThrow(CompileErrorException::new)) {
@@ -49,6 +79,14 @@ public class Compiler extends AbstractNodeDiagnosticEmitter {
         }
     }
 
+    /**
+     * Compiles a single statement.
+     *
+     * @param node the statement AST node to compile
+     * @return a compiled {@link Stmt statement}
+     * @throws CompileErrorException in case of semantic errors
+     * @see #compile(ProgramNode)
+     */
     public Stmt compile(StmtNode node) throws CompileErrorException {
         try (DiagnosticCollectorCloseable<CompileErrorException> diagnosticsToThrow =
                      collectDiagnosticsToThrow(CompileErrorException::new)) {
@@ -60,12 +98,25 @@ public class Compiler extends AbstractNodeDiagnosticEmitter {
         }
     }
 
+    /**
+     * Naked version of {@link #compile(ProgramNode)} that doesn't throw errors.
+     *
+     * @param node a {@link ProgramNode root} node to compile
+     * @return a list of compiled statements, some of that may be {@link Stmt#isComplete() incomplete} (in case of
+     * diagnostic errors)
+     */
     protected List<Stmt> compileProgram(ProgramNode node) {
         return node.getStmts().stream()
                 .map(this::compileStmt)
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Naked version of {@link #compile(StmtNode)} that doesn't throw errors.
+     *
+     * @param node a single {@link StmtNode statement} node to compile
+     * @return a compiled statement, {@link Stmt#isComplete() incomplete} in case of diagnostic errors
+     */
     protected Stmt compileStmt(StmtNode node) {
         return new NodeVisitor<Stmt>() {
             @Override
@@ -110,13 +161,27 @@ public class Compiler extends AbstractNodeDiagnosticEmitter {
         Variable outputVariable = new Variable(outputVariableName, exprTypeInfo.getExprType());
 
         return new Stmt(exprFunction, inputVariables, outputVariable);
-
     }
 
+    /**
+     * Invokes the {@link TypeInferrer type inferrer} setting it up to pass its diagnostics through this instance.
+     *
+     * @param node a node holding an expression to infer types for; usually some {@link StmtNode statement}
+     * @return an {@link ExprTypeInfo} instance filled in, {@link ExprTypeInfo#isComplete() incomplete} in case of type
+     * errors
+     */
     protected ExprTypeInfo inferTypeInfo(ExprHolderNode node) {
         return typeInferrer.inferWithDiagnosticListener(node, globalVariableMap, this::emitDiagnostic);
     }
 
+    /**
+     * Given a {@link ExprTypeInfo#isComplete() complete} {@link ExprTypeInfo} instance, invokes the {@link ExprBuilder
+     * expression builder} that assembles the expression into a callable function.
+     *
+     * @param exprTypeInfo the expression type info
+     * @return a callable
+     * @throws IllegalArgumentException if the exprTypeInfo is {@link ExprTypeInfo#isComplete() incomplete}
+     */
     protected Function<Object[], ?> buildExprFunction(ExprTypeInfo exprTypeInfo) {
         if (!exprTypeInfo.isComplete()) {
             throw new IllegalArgumentException("!exprTypeInfo.isComplete()");

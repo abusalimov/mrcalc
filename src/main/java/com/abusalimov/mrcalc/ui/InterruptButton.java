53d598f2beec3e5065d89d792897fce5f90cf860
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2016-07-08 01:40:27 +0300

    ui: Invoke "interrupt" button listener on EDT

--- src/main/java/com/abusalimov/mrcalc/ui/InterruptButton.java
+++ src/main/java/com/abusalimov/mrcalc/ui/InterruptButton.java
@@ -11,6 +11,6 @@ public class InterruptButton extends JButton {
     public InterruptButton(CalcExecutor calcExecutor) {
         super("Interrupt");
         addActionListener(e -> calcExecutor.cancel());
-        calcExecutor.addAndFireExecutionListener(this::setEnabled);
+        calcExecutor.addAndFireExecutionListener(enabled -> SwingUtilities.invokeLater(() -> setEnabled(enabled)));
     }
 }
